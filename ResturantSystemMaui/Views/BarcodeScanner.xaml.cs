8463f106db1b581263f08bb35553588d3062a67b
A00246505 <90653464+A00246505@users.noreply.github.com>
2022-12-14 14:39:20 +0500

    Order item using barcode added

--- ResturantSystemMaui/Views/BarcodeScanner.xaml.cs
+++ ResturantSystemMaui/Views/BarcodeScanner.xaml.cs
@@ -2,6 +2,7 @@ namespace ResturantSystemMaui.Views;
 
 public partial class BarcodeScanner : ContentPage
 {
+    string tableno = "";
 	public BarcodeScanner()
 	{
 		InitializeComponent();
@@ -15,7 +16,9 @@ public partial class BarcodeScanner : ContentPage
 		{
 
             barcodeResult.Text = e.Results[0].Value.ToString();
-
+            tableno= e.Results[0].Value.ToString();
+            tableno = tableno.Split(',')[0];
+           
 
         });
 
@@ -23,7 +26,7 @@ public partial class BarcodeScanner : ContentPage
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-
+        result.Text= "Ordered for "+ tableno;
     }
 
     private async void ToolbarItem_Clicked(object sender, EventArgs e)
